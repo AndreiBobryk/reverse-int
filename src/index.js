94daabc54bb638a4c344abfb3f19a43ea32b8126
Andrei Bobryk <andreibobryk1@gmail.com>
2022-01-07 11:28:55 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    const m = n.toString().split('').reverse().join('')
+  
+    if (n<0) {
+        return Number(m.slice(0, m.length-1));
+    } else {
+        return Number(m);
+    }
   
 }
+
+
+
